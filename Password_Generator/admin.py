1e009393e3a3877ba48a65f698a209b4d0e64b92
AjmalKBangash <ak1489007@gmail.com>
2023-07-11 12:05:43 +0430

    sessions added with recently generated passwords

--- Password_Generator/admin.py
+++ Password_Generator/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.sessions.models import Session
 
 # Register your models here.
+
+# if you want to save decoded data in django-admin
+# class SessionAdmin(ModelAdmin):
+#     def _session_data(self, obj):
+#         return obj.get_decoded()
+#     list_display = ['session_key', '_session_data', 'expire_date']
+# admin.site.register(Session, SessionAdmin)
+admin.site.register(Session)
+
--- Password_Generator/views.py
+++ Password_Generator/views.py
@@ -7,12 +7,15 @@ import random
 
 # Create your views here.
 def pass_generate(request):
+    # Getting data from form
     length = request.GET.get('Length')
     uppercase = request.GET.get('uppercase')
     lowercase = request.GET.get('lowercase')
     specialcharacters = request.GET.get('specialcharacters')
     numbers = request.GET.get('numbers')
     
+    
+    #Generating Password
     listt = ['-','_','*','$','@','!','#','%']
     listtt = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     def upperCase():
@@ -40,14 +43,26 @@ def pass_generate(request):
             if numbers and add <= int(length):
                 result += str(numbeR())
                 add = add + 1
+            
+    # Sending data to sessions
+
+    if 'last_15' in request.session: 
+        if len(request.session['last_15']) > 14:
+            request.session['last_15'].pop()
+        if result:
+            request.session['last_15'].insert(0,result)
+    else:
+        request.session['last_15'] = [result]
+    # for i in  
+    sessions_result = request.session['last_15']
+    request.session.modified = True # We are telling django that every time the page reloads or this sessions code runs you should modify list inside your sessions everytime, django doesn't modify list and dictionnaries inside sessions on every reload that is why we have to tell django to modify lists,dicts inside sessions through this command
+
 
     
     # def fun():
     #     return (listtt[random.randint(0,25)] + listtt[random.randint(0,25)] + listt[random.randint(0,7)] + listtt[random.randint(0,25)])
-    
-    
     # print(fun() + str(random.randint(0,99)) + fun() + str(random.randint(1,99)) )
     # varr = fun() + str(random.randint(0,99)) + fun() + str(random.randint(1,99)) 
     
-    return render(request, 'pass-generator.html', {'result': result})
+    return render(request, 'pass-generator.html', {'result': result, 'sessions_result': sessions_result})
     # return HttpResponse('<span>Salam</span><span>W.Salam</span>')
